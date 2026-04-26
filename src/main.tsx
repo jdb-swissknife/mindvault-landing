9436365338b8531e28bf55d1887c50503064e29e
John Rice <john@agenticmsp.com>
2026-04-26 20:53:32 +0000

    Fix prerender hydration: clear root before React mounts

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Router from './Router'
 
-createRoot(document.getElementById('root')!).render(
+const rootEl = document.getElementById('root')!
+rootEl.innerHTML = '' // Clear prerendered content to avoid hydration mismatch
+createRoot(rootEl).render(
   <StrictMode>
     <Router />
   </StrictMode>,
